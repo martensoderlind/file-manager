e6afbcafae102d4e0e181a0fd9e83526cc41bf60
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-27 14:07:44 +0100

    setting up project

---
+++ build/CMakeFiles/file_manager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_manager.dir/src/main.cpp.o"
+  "CMakeFiles/file_manager.dir/src/main.cpp.o.d"
+  "file_manager"
+  "file_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/file_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "file manager" << endl;
+    return 0;
+}
