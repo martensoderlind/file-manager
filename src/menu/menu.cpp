a1f173a07a785ba935ed9caf00681fa407a91848
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-03-31 09:30:06 +0200

    removed main menu

--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -28,45 +28,7 @@ void Menu::stateChange(const int &option)
         break;
     }
 }
-// int Menu::mainMenu()
-// {
-//     int choice = 0;
-//     int key;
-//     bool run = true;
-//     const char *options[] = {"List files", "Create file", "Create directory", "Delete File", "Exit"};
-//     int num_options = sizeof(options) / sizeof(options[0]);
 
-//     while (run)
-//     {
-//         for (int i = 0; i < num_options; i++)
-//         {
-//             if (i == choice)
-//             {
-//                 attron(A_REVERSE);
-//             }
-//             mvprintw(i + 2, 2, "%s", options[i]);
-//             attroff(A_REVERSE);
-//         }
-//         mvprintw(1, 2, "=====FILE MANAGER=====");
-//         refresh();
-//         key = getch();
-
-//         switch (key)
-//         {
-//         case KEY_UP:
-//             choice = (choice - 1 + num_options) % num_options;
-//             break;
-//         case KEY_DOWN:
-//             choice = (choice + 1) % num_options;
-//             break;
-//         case '\n':
-//             run = false;
-//             endwin();
-//             return choice;
-//         }
-//     }
-//     return -1;
-// }
 int Menu::displayMenu(const std::vector<DirectoryEntry> &entries)
 {
     int choice = 0;
